73eddcc4ccb2194fdc1e3e64d7ee889ad5808902
nico-gg <39880772+nico-gg@users.noreply.github.com>
2025-08-23 14:13:45 -0400

    Tooling (#3)
    * correct img name
    
    * tooling kit
    
    * cleanup
    
    * clean script
    
    * fix cleanup
    
    * Update 03_diatonic_motions.md

---
+++ tooling/make_examples.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+
+
+def make_file_name(author, work, measure):
+    return '-'.join(['_'.join(author.split()), 
+                      "_".join(work.split() + measure.split())])
+
+
+examples_df = pd.read_csv("tooling\\\\examples.csv", index_col=False)
+
+for i, folder, author, work, measure, text in examples_df.itertuples():
+    image_name = make_file_name(author, work, measure)
+    full_path = f"images\\\\{folder}\\\\{image_name}.png"
+    
+    try: 
+        os.rename(f"tooling\\\\example_images\\\\{i}.png", full_path)
+    except FileExistsError:
+        print(f"could not write {full_path}")
+    except FileNotFoundError:
+        print(f"could not find image {i}")
+
+    with open("tooling\\\\examples_output.md", "a") as f:
+        f.write(
+            f"""
+            #### {folder}
+
+            - **{author}, {work} {measure}:** {text} ![]({full_path})
+            """
+        )
+
+
+
