77936ce0e274c0c7e3eb5be6730af1048385cf17
nico-gg <nic.godbout@gmail.com>
2025-08-24 11:04:24 -0400

    fix spacing

--- tooling/make_examples.py
+++ tooling/make_examples.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 
-
 def make_file_name(author, work, measure):
     return '-'.join(['_'.join(author.split()), 
                       "_".join(work.split() + measure.split())])
@@ -12,7 +11,7 @@ examples_df = pd.read_csv("tooling\\\\examples.csv", index_col=False)
 for i, folder, author, work, measure, text in examples_df.itertuples():
     image_name = make_file_name(author, work, measure)
     full_path = f"images\\\\{folder}\\\\{image_name}.png"
-    
+
     try: 
         os.rename(f"tooling\\\\example_images\\\\{i}.png", full_path)
     except FileExistsError:
@@ -20,16 +19,8 @@ for i, folder, author, work, measure, text in examples_df.itertuples():
     except FileNotFoundError:
         print(f"could not find image {i}.png")
 
+    online_path = "../" + full_path.replace('\\\\', '/')
     with open("tooling\\\\examples_output.md", "a") as f:
-        f.write(
-            f"""
-            # {folder}
-
-            - **{author}, {work} {measure}:** {text} 
-            
-              ![](../{full_path.replace('\\\\', '/')})
-            """
-        )
-
-
-
+        f.write(f"# {folder} \n\n")
+        f.write(f"- **{author}, {work} {measure}:** {text} \n\n")
+        f.write(f"  ![]({online_path}) \n\n")
